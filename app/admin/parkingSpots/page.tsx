100d47791896aef0c30e78137f2f7de33ff1a17c
joniashimi10 <jashimi@rpay.ai>
2026-06-12 21:46:06 +0200

    =Revert color changes

--- app/admin/parkingSpots/page.tsx
+++ app/admin/parkingSpots/page.tsx
@@ -391,7 +391,7 @@ export default function ParkingSpotsConfig() {
                                     />
                                 </div>
 
-                                <div className="bg-red-800 relative z-[1000] w-full">
+                                <div className="relative z-[1000] w-full">
                                     <label className="block text-sm font-medium text-gray-700 mb-1">
                                         Spot Category Type (Linked Rates)
                                     </label>
@@ -407,7 +407,7 @@ export default function ParkingSpotsConfig() {
 
                                     {isTypeDropdownOpen && (
                                         <div
-                                            className="bg-blue-500 absolute z-[999] w-full mt-1 bg-white border border-gray-300 rounded-xl shadow-lg max-h-60 overflow-auto">
+                                            className="absolute z-[999] w-full mt-1 bg-white border border-gray-300 rounded-xl shadow-lg max-h-60 overflow-auto">
                                             {categories.map((category) => (
                                                 <div
                                                     key={category.id}
